3715dfe88e9489a610b18361cd180ed3ec3c67f2
slarsar <slarsar@yandex.com>
2019-07-19 23:25:47 +0800

    hamming distance pause process

--- hamming/hamming.scm
+++ hamming/hamming.scm
@@ -1,17 +1,15 @@
 (define-module (hamming)
   #:export (hamming-distance))
-(define hamming (lambda (list1 list2)
-(
-			   (display list1)
-			   (newline)
-			     (if (null? (car list1)) 0    
-				 (if ( equal?  (car list1 ) (car list2)  ) 0
-				      (+ 1 (hamming (cdr list1) (cdr list2))) 
-				  )
-			   )
-)
-))
+(define hamming-distance
+  (lambda (list1 list2)
+    (
+     if (equal? list1 '())
+     0
+     (if (equal? (car list1) (car list2))
+	  (+ 0 (hamming-distance (cdr list1) (cdr list2)))
+	  (+ 1 (hamming-distance (cdr list1) (cdr list2)))
+	  ))))
 
 
-
-(display hamming '(A C D)  '(A B D))
+(display (hamming-distance '(A C D) '(A B D)))
+(newline)
